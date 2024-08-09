9fd9bc4342742c41495db8b2decc784abef6caeb
seungjo <f1v3@kakao.com>
2024-08-09 19:43:22 +0900

    refactor: Long(BigInteger) -> BigDecimal 변경

--- src/main/java/com/lemontree/interview/request/MemberCreate.java
+++ src/main/java/com/lemontree/interview/request/MemberCreate.java
@@ -4,6 +4,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
+import java.math.BigDecimal;
+
 /**
  * 유저 생성 Request 클래스입니다.
  *
@@ -13,20 +15,20 @@ import lombok.Getter;
 @Getter
 public class MemberCreate {
 
-    @NotBlank
+    @NotBlank(message = "이름을 입력해주세요.")
     private String name;
 
-    @NotNull
-    private Long balance;
+    @NotNull(message = "잔액을 입력해주세요.")
+    private BigDecimal balance;
 
-    @NotNull
-    private Long onceLimit;
+    @NotNull(message = "1회 한도를 입력해주세요.")
+    private BigDecimal onceLimit;
 
-    @NotNull
-    private Long dailyLimit;
+    @NotNull(message = "1일 한도를 입력해주세요.")
+    private BigDecimal dailyLimit;
 
-    @NotNull
-    private Long monthlyLimit;
+    @NotNull(message = "1달 한도를 입력해주세요.")
+    private BigDecimal monthlyLimit;
 
     private Boolean isDeleted;
 }
